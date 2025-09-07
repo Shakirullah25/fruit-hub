34794b5eab2d67d525218d5a89463df5a50e04a9
Shakirullah <omotososakiru25@gmail.com>
2025-09-07 17:54:08 +0100

    feat: created and adding the add to basket screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:fruit_salad_combo/constant/colors.dart';
+import 'package:fruit_salad_combo/screens/add_to_basket_screen.dart';
 import 'package:fruit_salad_combo/screens/home_screen.dart';
 import 'package:fruit_salad_combo/screens/welcome_screen.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -29,7 +30,7 @@ class MyApp extends StatelessWidget {
         home: child,
       );
     },
-    child: HomeScreen(),
+    child: AddToBasketScreen(),
   );
   // @override
   // Widget build(BuildContext context) {
---
+++ lib/screens/add_to_basket_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:fruit_salad_combo/constant/colors.dart';
+
+class AddToBasketScreen extends StatelessWidget {
+  const AddToBasketScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SafeArea(
+      child: Scaffold(
+        backgroundColor: AppColors.scaffoldColor,
+        
+      ),
+    );
+  }
+}
