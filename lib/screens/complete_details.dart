0ea20496b351367c4cd1bd76e4fffebf65907e08
Shakirullah <omotososakiru25@gmail.com>
2025-09-10 03:02:06 +0100

    fixes: added close icon to the bottom modal sheet

--- lib/screens/complete_details.dart
+++ lib/screens/complete_details.dart
@@ -12,66 +12,103 @@ void showCheckoutSheet(BuildContext context) {
     shape: RoundedRectangleBorder(
       borderRadius: BorderRadius.vertical(top: Radius.circular(20.r)),
     ),
+    isScrollControlled: true,
     builder: (context) {
-      return SizedBox(
-        width: double.infinity,
-        child: Padding(
-          padding: EdgeInsets.only(top: 0.04.sh, left: 0.08.sw, right: 0.08.sw),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                MyStrings.deliveryAddress,
-                style: TextStyle(
-                  fontSize: 20.spMin,
-                  fontWeight: FontWeight.w500,
-                  color: AppColors.secondaryColor,
-                ),
+      return Stack(
+        clipBehavior: Clip.none,
+        children: [
+          SizedBox(
+            width: double.infinity,
+            child: Padding(
+              padding: EdgeInsets.only(
+                top: 0.04.sh,
+                left: 0.08.sw,
+                right: 0.08.sw,
               ),
-              SizedBox(height: 0.03.sh),
-              PrimaryTextField(hintText: MyStrings.hintDeliveryAddress),
-              //SizedBox(height: 0.03.sh),
-              Padding(
-                padding: EdgeInsets.only(bottom: 0.03.sh),
-                child: Text(
-                  MyStrings.no2Call,
-                  style: TextStyle(
-                    fontSize: 20.spMin,
-                    fontWeight: FontWeight.w500,
-                    color: AppColors.secondaryColor,
-                  ),
-                ),
-              ),
-              PrimaryTextField(hintText: MyStrings.numberHint),
-              SizedBox(height: 0.03.sh),
-              Row(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Expanded(
-                    child: PrimaryButton(
-                      label: MyStrings.payOnDelivery,
-                      backgroundColor: AppColors.scaffoldColor,
-                      foregroundColor: AppColors.primaryColor,
-                      onPressed: () {},
-                      borderSide: BorderSide(color: AppColors.primaryColor),
+                  Text(
+                    MyStrings.deliveryAddress,
+                    style: TextStyle(
+                      fontSize: 20.spMin,
+                      fontWeight: FontWeight.w500,
+                      color: AppColors.secondaryColor,
                     ),
                   ),
-                  SizedBox(width: 0.05.sw), // space between buttons
-                  Expanded(
-                    child: PrimaryButton(
-                      onPressed: () {},
-                      label: MyStrings.payWithCard, // 👈 give this a diff label
-                      backgroundColor: AppColors.scaffoldColor,
-                      foregroundColor: AppColors.primaryColor,
-                      borderSide: BorderSide(color: AppColors.primaryColor),
+                  SizedBox(height: 0.03.sh),
+                  PrimaryTextField(hintText: MyStrings.hintDeliveryAddress),
+                  //SizedBox(height: 0.03.sh),
+                  Padding(
+                    padding: EdgeInsets.only(bottom: 0.03.sh),
+                    child: Text(
+                      MyStrings.no2Call,
+                      style: TextStyle(
+                        fontSize: 20.spMin,
+                        fontWeight: FontWeight.w500,
+                        color: AppColors.secondaryColor,
+                      ),
                     ),
                   ),
+                  PrimaryTextField(hintText: MyStrings.numberHint),
+                  SizedBox(height: 0.03.sh),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: PrimaryButton(
+                          label: MyStrings.payOnDelivery,
+                          backgroundColor: AppColors.scaffoldColor,
+                          foregroundColor: AppColors.primaryColor,
+                          onPressed: () {},
+                          borderSide: BorderSide(color: AppColors.primaryColor),
+                        ),
+                      ),
+                      SizedBox(width: 0.05.sw), // space between buttons
+                      Expanded(
+                        child: PrimaryButton(
+                          onPressed: () {},
+                          label: MyStrings
+                              .payWithCard, // 👈 give this a diff label
+                          backgroundColor: AppColors.scaffoldColor,
+                          foregroundColor: AppColors.primaryColor,
+                          borderSide: BorderSide(color: AppColors.primaryColor),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 0.04.sw), // space between buttons
                 ],
               ),
-              SizedBox(height: 0.04.sw), // space between buttons
-            ],
+            ),
           ),
-        ),
+
+          // -----> Top Close Icon in a Stack Widget <-----
+          Positioned(
+            top: -0.07.sh, // negative to make it float outside sheet
+            left: 0,
+            right: 0,
+            child: Center(
+              child: InkWell(
+                onTap: () => Navigator.pop(context),
+                child: Container(
+                  width: 48.w,
+                  height: 48.w,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: AppColors.scaffoldColor,
+                  ),
+                  child: Icon(
+                    Icons.close,
+                    color: AppColors.secondaryColor,
+                    size: 24.spMin,
+                    weight: 100,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       );
     },
   );
