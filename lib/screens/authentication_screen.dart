4720b0b524454b184db5615266d972f87b7ecd69
Shakirullah <omotososakiru25@gmail.com>
2025-09-14 10:52:09 +0100

    refactor: authentication screen to handle validation and navigation to maiin screen

--- lib/screens/authentication_screen.dart
+++ lib/screens/authentication_screen.dart
@@ -1,13 +1,63 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:fruit_salad_combo/screens/main_screen.dart';
 import 'package:fruit_salad_combo/widgets/primary_button.dart';
 import 'package:fruit_salad_combo/constant/colors.dart';
 import 'package:fruit_salad_combo/widgets/container.dart';
 import 'package:fruit_salad_combo/constant/my_strings.dart';
 import 'package:fruit_salad_combo/widgets/primary_textfield.dart';
 
-class AuthenticationScreen extends StatelessWidget {
+class AuthenticationScreen extends StatefulWidget {
   const AuthenticationScreen({super.key});
+
+  @override
+  State<AuthenticationScreen> createState() => _AuthenticationScreenState();
+}
+
+class _AuthenticationScreenState extends State<AuthenticationScreen> {
+  final TextEditingController _firstNameController = TextEditingController();
+  String? _errorMessage;
+
+  @override
+  void dispose() {
+    _firstNameController;
+    super.dispose();
+  }
+
+  void _authenticate() {
+    final String name = _firstNameController.text.trim();
+
+    // Reset error message
+
+    setState(() {
+      _errorMessage = null;
+    });
+
+    if (name.isEmpty) {
+      setState(() {
+        _errorMessage = "Name cannot be empty.";
+      });
+    } else if (name.length > 25) {
+      setState(() {
+        _errorMessage = "Name is too long.";
+      });
+    } else if (!RegExp(r"^[a-zA-Z\s]+$").hasMatch(name)) {
+      setState(() {
+        _errorMessage = "Please use only letters and spaces.";
+      });
+    } else {
+        Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => MainScreen()),
+      );
+    }
+  }
+
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   _authenticate();
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -37,14 +87,31 @@ class AuthenticationScreen extends StatelessWidget {
                   SizedBox(height: 0.02.sh),
                   Padding(
                     padding: EdgeInsets.only(right: 0.1.sw),
-                    child: PrimaryTextField(hintText: MyStrings.authHintTxt),
+                    child: PrimaryTextField(
+                      hintText: MyStrings.authHintTxt,
+                      textEditingController: _firstNameController,
+                      keyboardType: TextInputType.name,
+                    ),
                   ),
+
+                  // Displaying the error message if thats not null
+                  if (_errorMessage != null)
+                  Padding(
+                      padding: EdgeInsets.only(top: 0.01.sh, right: 0.1.sw),
+                      child: Text(
+                        _errorMessage!,
+                        style: TextStyle(
+                          color: AppColors.primaryColor,
+                          fontSize: 12.spMin,
+                        ),
+                      ),
+                    ),
                   SizedBox(height: 0.05.sh),
                   Padding(
                     padding: EdgeInsets.only(right: 0.08.sw),
                     child: PrimaryButton(
                       label: MyStrings.authButtonTxt,
-                      onPressed: () => AuthenticationScreen(),
+                      onPressed: () => _authenticate(),
                     ),
                   ),
                 ],
