50b13a9e8123260a4ae180fda1f5d0829b316ed1
Shakirullah <omotososakiru25@gmail.com>
2025-09-18 18:26:46 +0100

    chore: changed the targetndkVersion from 34 to 36 and other things

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.example.fruit_salad_combo"
-    compileSdk = 34
+    compileSdk = 36
     ndkVersion = "27.0.12077973"
     // compileSdk = flutter.compileSdkVersion
     // ndkVersion = flutter.ndkVersion
@@ -26,8 +26,8 @@ android {
         applicationId = "com.example.fruit_salad_combo"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 21
-        targetSdk = 34
+        minSdk = flutter.minSdkVersion
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
     }
