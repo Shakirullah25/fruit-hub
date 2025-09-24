1a706261d83be8f4983af5794ff24c5974dda757
Shakirullah <omotososakiru25@gmail.com>
2025-09-25 00:04:14 +0100

    fix: removed the top padding of the fruit basket container

--- lib/widgets/container.dart
+++ lib/widgets/container.dart
@@ -15,37 +15,31 @@ class FruitBasketContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.only(
-        topLeft: Radius.circular(16.r),
-        topRight: Radius.circular(16.r),
-      ),
-      child: Container(
-        width: 1.sw,
-        height: 0.5.sh, // Takes 50% of the screen size
-        color: AppColors.primaryColor,
-        child: Column(
-          mainAxisAlignment: imgMainAxisAlignment,
-          children: [
+    return Container(
+      width: 1.sw,
+      height: 0.5.sh, // Takes 50% of the screen size
+      color: AppColors.primaryColor,
+      child: Column(
+        mainAxisAlignment: imgMainAxisAlignment,
+        children: [
+          Image.asset(
+            basketImgPath,
+            //MyStrings.basketImgPath,
+            width: 0.8.sw,
+            height: 0.3.sh,
+            fit: BoxFit.contain,
+          ),
+          SizedBox(height: 0.02.sh),
+          if (shadowImgPath != null)
             Image.asset(
-              basketImgPath,
-              //MyStrings.basketImgPath,
+              shadowImgPath!,
+              //MyStrings.shadowImgPath,
               width: 0.8.sw,
-              height: 0.3.sh,
+              height: 0.015.sh,
               fit: BoxFit.contain,
             ),
-            SizedBox(height: 0.02.sh),
-            if (shadowImgPath != null)
-              Image.asset(
-                shadowImgPath!,
-                //MyStrings.shadowImgPath,
-                width: 0.8.sw,
-                height: 0.015.sh,
-                fit: BoxFit.contain,
-              ),
-            SizedBox(height: 0.02.sh),
-          ],
-        ),
+          SizedBox(height: 0.02.sh),
+        ],
       ),
     );
   }
